95b8b713bd240e0f266ee9f92eddc2ab20160c8b
FernandoYa <47068097+FernandoYa@users.noreply.github.com>
2022-11-21 23:41:54 +0100

    fourth edit

--- registercontrol.js
+++ registercontrol.js
@@ -1,18 +1,49 @@
-var isAlpha = function(ch){
-    return typeof ch === "string" ;
+function isAlpha(ch) {
+    if (isNaN(ch)==false || ch.length<1 ) {
+
+       return false;
+    }
+    ch=ch.toUpperCase();
+    for (let i=0;i<ch.length;i++) {
+        if ((ch[i]<"A")||(ch[i]>"Z"))  {
+            return false;
+        }
+    }
+    return true;
+
 }
 
+function passcheck() {
+    var password=document.getElementById("register-password");
+    if (password.value=="1")  {
+        return false
+    }
+    else {
+        return true
+    }
+}
 function register() {
-    var name=document.forms["register"]["r-name"]
-    var forname=document.getElementById("register-last-name").value
-    var email=document.getElementById("register-email").value
-    var password=document.getElementById("register-password").value
-    var password=document.getElementById("register-password").value
-    var password2=document.getElementById("register-password-2").value
+    var name=document.getElementById("register-name").value;
+    var forname=document.getElementById("register-last-name").value;
 
-    if(name.value=="1") {
-        alert("wrong name")
+    if(isAlpha(name)==false) {
+        document.getElementById("wrongwrite").innerHTML="Wrong Name";
+        return false;
+    }  
+    else {
+        document.getElementById("wrongwrite").innerHTML="";
+    }
+    if(isAlpha(forname)==false) {
+        document.getElementById("wrong-forname").innerHTML="Wrong Forname";
+        return false;
+    } 
+    else {
+        document.getElementById("wrong-forname").innerHTML="";
+    } 
+    if (passcheck(password)==false) {
+        document.getElementById("wrongpass").innerHTML="Type a Valid Password";
         return false;
     }
+
 
 }
